60a2d3da61070b31dd68d2a9cb741eafd8319463
Fredrik Löfgren <fr3drik.l@gmail.com>
2023-03-27 13:50:49 +0200

    chore: add formating

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,18 +1,25 @@
 import { useLoaderData } from "@remix-run/react";
 
 export async function loader() {
-  const res = await fetch(`https://api.sl.se/api2/realtimedeparturesV4.json?key=${process.env.REALTIME_KEY}&siteid=9733&timewindow=30`).then((res) => res.json());
-  console.log(process.env.REALTIME_KEY, '<--- key')
-  console.log(res, '<--- YOLO')
-  return {status: "yolo"}
+  return await fetch(`https://api.sl.se/api2/realtimedeparturesV4.json?key=${process.env.REALTIME_KEY}&siteid=9733&timewindow=60`).then((res) => res.json());
 }
 
 export default function Index() {
-  const yolo = useLoaderData()
-  console.log(yolo, '<--- WTF')
+  const {ResponseData} = useLoaderData()
+  const trains = ResponseData.Trains
+  const cityBound = trains.filter(train => train.JourneyDirection === 2)
+  const nonCityBound = trains.filter(train => train.JourneyDirection === 1)
+  console.log(nonCityBound, '<--- WTF')
   return (
     <div>
-    <h1>YOLO</h1>
+      <h2>Mot stan</h2>
+    <ul>
+      {cityBound.map(train => <li key={train.JourneyNumber}>{train.LineNumber} {train.Destination} {train.DisplayTime}</li>)}
+    </ul>
+          <h2>Bort från stan</h2>
+    <ul>
+      {nonCityBound.map(train => <li key={train.JourneyNumber}>{train.LineNumber} {train.Destination} {train.DisplayTime}</li>)}
+    </ul>
     </div>
   );
 }
