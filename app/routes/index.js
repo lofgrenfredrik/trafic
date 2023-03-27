517e80fbd32efcb3c29791f7a20ca9561e602e6e
Fredrik Löfgren <fr3drik.l@gmail.com>
2023-03-27 16:10:25 +0200

    feat: tweaks

--- app/routes/index.js
+++ app/routes/index.js
@@ -20,6 +20,21 @@ function deviationsStatus(deviations, journeyNumber) {
   return null
 }
 
+function departureItems(item) {
+  return (
+    <li
+      className=" flex justify-between px-2 py-1 text-lg text-white odd:bg-black/25"
+      key={item.JourneyNumber}
+    >
+      <span>
+        {item.LineNumber} {item.Destination}
+      </span>
+      <span>{deviationsStatus(item.Deviations, item.JourneyNumber)}</span>
+      <span className="">{item.DisplayTime}</span>
+    </li>
+  )
+}
+
 export default function Index() {
   const { ResponseData } = useLoaderData()
   const trains = ResponseData.Trains
@@ -28,42 +43,21 @@ export default function Index() {
   const nonCityBound = trains.filter((train) => train.JourneyDirection === 1)
 
   return (
-    <div className="mx-16 flex flex-col items-center justify-center gap-6">
+    <div className="flex flex-col items-center justify-center gap-6 lg:mx-16">
       <h1 className="py-7 font-serif text-5xl font-bold">Vega station</h1>
-      <div className="flex w-full flex-col gap-6 bg-sky-500 p-3">
+      <div className="flex w-full max-w-[700px] flex-col gap-6 bg-sky-500 p-3">
         <div>
           <h2 className="text-2xl font-bold">Mot stan</h2>
-          <ul>
-            {cityBound.map((train) => (
-              <li className="p-1 text-white odd:bg-black/25" key={train.JourneyNumber}>
-                {train.LineNumber} {train.Destination} {train.DisplayTime}
-                {deviationsStatus(train.Deviations, train.JourneyNumber)}
-              </li>
-            ))}
-          </ul>
+          <ul>{cityBound.map((train) => departureItems(train))}</ul>
         </div>
         <div>
           <h2 className="text-2xl font-bold">Söderut</h2>
-          <ul>
-            {nonCityBound.map((train) => (
-              <li className="p-1 text-white odd:bg-black/25" key={train.JourneyNumber}>
-                {train.LineNumber} {train.Destination} {train.DisplayTime}
-                {deviationsStatus(train.Deviations, train.JourneyNumber)}
-              </li>
-            ))}
-          </ul>
+          <ul>{nonCityBound.map((train) => departureItems(train))}</ul>
         </div>
       </div>
-      <div className="flex w-full flex-col bg-red-500 p-3">
-        <h2 className="text-2xl font-bold">Bussar</h2>
-        <ul>
-          {buses.map((bus) => (
-            <li className="p-1 text-white odd:bg-black/25" key={bus.JourneyNumber}>
-              {bus.LineNumber} {bus.Destination} {bus.DisplayTime}
-              {deviationsStatus(bus.Deviations, bus.JourneyNumber)}
-            </li>
-          ))}
-        </ul>
+      <div className="flex w-full max-w-[700px] flex-col bg-red-500 p-3">
+        <h2 className="text-2xl font-bold">Buss 837</h2>
+        <ul>{buses.filter((bus) => bus.LineNumber === "837").map((bus) => departureItems(bus))}</ul>
       </div>
     </div>
   )
